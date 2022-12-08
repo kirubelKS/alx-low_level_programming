7fcc1a560a14c4d26045fdc8db99cd0856c5ad0a
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2022-12-08 15:54:44 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Return: Always 0 (Success) 
-  */
-
+ * Return: Always 0 (sucess)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-
+	printf("with proper grammer, but the outcome is a piece or art,\n)"
+	return (0);
 }
