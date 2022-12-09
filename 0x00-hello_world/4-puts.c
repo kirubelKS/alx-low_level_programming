c2049ea8964d5133ff086d3f46fd9aee82d4d4d0
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2022-12-08 18:00:14 -0800

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *
+
  *  * main - Entry point
  *
  *   *
@@ -8,7 +8,7 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-int main(void)
+	int main(void) 
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
