44b8fe5b9858359a6bf583e9135d5ed7d405dac3
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2022-12-08 17:42:46 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 
 
 /**
  *
- *  * main - Entry point
+ *  * main - prints exactly and that piece of art is useful
  *
  *   *
  *
@@ -16,10 +18,11 @@ int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
+
+
 }
+
