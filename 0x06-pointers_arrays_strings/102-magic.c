dd57255bbd9bcbbe3b9360248e820a886033e881
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2022-12-21 23:22:17 +0300

    correction 1

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,80 +1,23 @@
-include "main.h"
-/**
- * *cap_string - function
- * @str: Pointer
- * Return: poniter return to function.
- */
+#include <stdio.h>
 
-char *cap_string(char *str)
+int main(void)
 {
-	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!',
-		'?', '"', '(', ')', '{', '}'};
-	int i, j;
+	int n;
+	int a[5];
+	int *p;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[0] >= 97 && str[0] <= 122)
-	{
-			str[0] = str[0] - 32;
-	}
-	for (j = 0; j < 14; j++)
-		if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == symb[j])
-			str[i] = str[i] - 32;
-	}
-	return (str);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this printsm 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
-
-
-
-
-
-
-
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-B
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-A
-
