c56c4d2f731f176c88a0073187ada762941e105b
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-03-23 13:45:24 +0300

    test 1

--- 0x18. C - Dynamic libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /temp/ https://raw.github.com/kirubelKS/alx-low_level_programminf/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
--- 0x18. C - Dynamic libraries/2-strncpy.c
+++ 0x18. C - Dynamic libraries/2-strncpy.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- *  *_strncpy - copies a string
- *   *@dest: A pointer to a character that will be changed
- *    *@src: A pointer to a character that will be changed
- *     *@n: value
- *      *Return: dest
- *       */
+ * _strncpy - Function that copies a string
+ * @dest: Pointer to a character that will be changed
+ * @src: Pointer to a character that will be changed
+ * @n: value
+ *
+ * Return: dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
 	int i;
 
 	i = 0;
+
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -26,5 +28,4 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 
 	return (dest);
-
 }
